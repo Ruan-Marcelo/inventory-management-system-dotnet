53d485ac5c6b82a1bcf557b5890a39d7cc63538f
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-23 16:41:24 -0300

    Create login auth com httpPost e tratamento de erros 409 e etc, login auth e logout criados

---
+++ AuthAPI/Controllers/UserAuthController.cs
@@ -0,0 +1,132 @@
+﻿using AuthAPI.Data;
+using AuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.CodeDom.Compiler;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAuthController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly string _jwtKey;
+        private readonly string? _jwtIssuer;
+        private readonly string? _jwtAudience;
+        private readonly int _JwtExpiry;
+
+        public UserAuthController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IConfiguration configuration)
+        {
+
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _jwtKey = configuration["Jwt:Key"];
+            _jwtIssuer = configuration["Jwt:Issuer"];
+            _jwtAudience = configuration["Jwt:Audience"];
+            _JwtExpiry = int.Parse(configuration["Jwt:ExpiryMinutes"]);
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            try
+            {
+                if (registerModel == null
+                    || string.IsNullOrEmpty(registerModel.Name)
+                    || string.IsNullOrEmpty(registerModel.Email)
+                    || string.IsNullOrEmpty(registerModel.Password))
+                {
+                    return BadRequest("Dados de registro inválidos");
+                }
+
+                var existingUser = await _userManager.FindByEmailAsync(registerModel.Email);
+                if (existingUser != null)
+                {
+                    //409
+                    return Conflict("E-mail já existe");
+                }
+
+                var user = new ApplicationUser
+                {
+                    UserName = registerModel.Email,
+                    Email = registerModel.Email,
+                    Name = registerModel.Name
+                };
+
+                var result = await _userManager.CreateAsync(user, registerModel.Password);
+
+                if (!result.Succeeded)
+                {
+                    return BadRequest(result.Errors);
+                }
+                //200
+
+                return Ok("Usuário criado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Erro interno no servidor ao criar usuário");
+            }
+
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByEmailAsync(loginModel.Email);
+            if (user == null)
+            {
+                return Unauthorized(new { success = false, message = "Nome de usuário ou senha inválidos" });
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginModel.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized(new { success = false, message = "Nome de usuário ou senha inválidos" });
+            }
+
+            var token = GeneratedJwtToken(user);
+            return Ok(new { success = true, token });
+        }
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok("User logged out successfully.");
+        }
+        private string GeneratedJwtToken(ApplicationUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub ,user.Id),
+                new Claim(JwtRegisteredClaimNames.Email , user.Email),
+                new Claim("Name" , user.Name),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                //issuer: _jwtIssuer,
+                //audience: _jwtAudience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_JwtExpiry),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
