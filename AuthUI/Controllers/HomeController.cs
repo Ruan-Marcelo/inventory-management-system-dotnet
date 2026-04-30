6ee79b7caab2f799262b8fd790c3694a151ae0b1
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-04-30 15:56:32 -0300

    Inicialização DashBoard para estoque de produto protegidos

--- AuthUI/Controllers/HomeController.cs
+++ AuthUI/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace AuthUI.Controllers
         {
             return View();
         }
+        public IActionResult Usuarios()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
